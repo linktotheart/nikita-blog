fd792790e9fdf40aef133d3e76720e7900ddaf37
Pulkit Singh <44489787+pulkitsingh69@users.noreply.github.com>
2020-12-20 07:38:00 +0530

    Index blue

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
+import PostLink from "../components/post-link"
 import HeroHeader from "../components/heroHeader"
 
 const IndexPage = ({
@@ -11,9 +12,9 @@ const IndexPage = ({
   },
 }) => {
 
-  // const Posts = edges
-  //   .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-  //   .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
   return (
     <Layout>
@@ -21,7 +22,9 @@ const IndexPage = ({
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
+      
       <HeroHeader/>
+
       {/* <h2> Projects  &darr;</h2>
       <div className="grids">
         {Posts}
@@ -29,7 +32,8 @@ const IndexPage = ({
       <div className="center" style={{textAlign: "center"}}>
       <Link to='/case' className="button -primary">View  All &rarr;</Link>
       </div> */}
-"
+
+
       <h2 className="mt-5">Brands I've worked with</h2>
       <div className="brands">
         <div className="brand">
@@ -88,27 +92,27 @@ const IndexPage = ({
 }
 
 export default IndexPage
-// export const pageQuery = graphql`
-//   query indexPageQuery {
-//     site {
-//       siteMetadata {
-//         title
-//         description
-//       }
-//     }
-//     allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] },limit: 6) {
-//       edges {
-//         node {
-//           id
-//           excerpt(pruneLength: 250)
-//           frontmatter {
-//             date(formatString: "MMMM DD, YYYY")
-//             path
-//             title
-//             thumbnail
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query indexPageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] },limit: 6) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+            thumbnail
+          }
+        }
+      }
+    }
+  }
+`
